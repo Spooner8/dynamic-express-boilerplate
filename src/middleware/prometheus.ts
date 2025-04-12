81d1eed06e974c0a1bdfc353176ab719d00bd647
Spooner8 <develop@swen-loeffler.ch>
2025-04-12 12:00:22 +0200

    update nginx configuration for metrics endpoint and adjust Prometheus job name and targets

--- src/middleware/prometheus.ts
+++ src/middleware/prometheus.ts
@@ -11,7 +11,7 @@ import promMid from 'express-prometheus-middleware';
  */
 export const prometheus = promMid({
     metricsPath: '/metrics',
-    collectDefaultMetrics: false,
+    collectDefaultMetrics: true,
     requestDurationBuckets: [0.1, 0.5, 1, 1.5],
     requestLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
     responseLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
