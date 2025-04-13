73548361ea6e68d61f4f4846d1358556fed46102
Spooner8 <develop@swen-loeffler.ch>
2025-04-13 14:32:49 +0200

    fix: refactor ESLint configuration to include globalIgnores and streamline file definitions

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,19 @@
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 
 export default defineConfig([
-  { 
-    files: ["**/*.{js,mjs,cjs,ts}"]
-  },
+  globalIgnores([
+    "**/node_modules/",
+    "**/dist/",
+    "**/prisma/",
+    "**/generated/",
+  ]),
   { 
     files: ["**/*.{js,mjs,cjs,ts}"],
-    languageOptions: { globals: globals.browser }
-  },
-  { 
-    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: globals.browser },
     plugins: { js },
     extends: ["js/recommended"]
   },
